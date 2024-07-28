79a38b8a717f0daab81f1ce40876d6531420d6fa
Nikosnakess <mario.facebook98@gmail.com>
2024-07-28 14:12:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Imposta la data di fine a 50 giorni da oggi
+let countDownDate = new Date();
+countDownDate.setDate(countDownDate.getDate() + 50);
+
+// Aggiorna il conto alla rovescia ogni secondo
+let x = setInterval(function() {
+    // Ottieni la data e l'ora attuali
+    let now = new Date().getTime();
+    
+    // Calcola la distanza tra adesso e la data di fine
+    let distance = countDownDate - now;
+    
+    // Calcola i giorni, ore, minuti e secondi
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Visualizza il risultato nell'elemento con id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // Se il conto alla rovescia è finito, scrivi un messaggio
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "CONTO ALLA ROVESCIA TERMINATO";
+    }
+}, 1000);
