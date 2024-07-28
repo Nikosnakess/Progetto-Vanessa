f88aceb93e201671b33208940eb17cc7f931eb8d
Nikosnakess <mario.facebook98@gmail.com>
2024-07-28 19:11:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 <script>
-    // Imposta la data di fine countdown solo la prima volta
-    if (!localStorage.getItem('countdownEndDate')) {
-        const targetDate = new Date();
-        targetDate.setDate(targetDate.getDate() + 49);
-        targetDate.setHours(targetDate.getHours() + 7);
-        targetDate.setMinutes(targetDate.getMinutes() + 30);
-        localStorage.setItem('countdownEndDate', targetDate);
-    }
+    // Calcola la data di fine countdown 49 giorni e 7 ore e 30 minuti dal momento attuale
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 49);
+    targetDate.setHours(targetDate.getHours() + 7);
+    targetDate.setMinutes(targetDate.getMinutes() + 30);
 
     function calculateTimeRemaining() {
-        const targetDate = new Date(localStorage.getItem('countdownEndDate'));
         const now = new Date();
         const timeDifference = targetDate - now;
 
